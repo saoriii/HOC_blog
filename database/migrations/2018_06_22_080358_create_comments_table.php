7fc052c3810964c923032677a508e7ddb3b055af
tareq-salem <38789773+tareq-salem@users.noreply.github.com>
2018-06-22 10:18:43 +0200

    Création de new table

---
+++ database/migrations/2018_06_22_080358_create_comments_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id');
+            $table->integer('is_active');
+            $table->string('content', 500);
+            $table->string('author', 100);
+            $table->string('email')->unique();
+
+
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
