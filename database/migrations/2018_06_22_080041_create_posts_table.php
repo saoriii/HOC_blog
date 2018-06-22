415d94b6cae84683bb0743871a6e04a1018910d1
saoriii <saori.kido07@gmail.com>
2018-06-22 12:16:18 +0200

    enlevement espace pour Camille

--- database/migrations/2018_06_22_080041_create_posts_table.php
+++ database/migrations/2018_06_22_080041_create_posts_table.php
@@ -20,7 +20,6 @@ class CreatePostsTable extends Migration
             $table->integer('user_id')->index()->unsigned();
             $table->integer('photo_id')->index()->unsigned();
             $table->integer('category_id')->index()->unsigned();
-
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
 
