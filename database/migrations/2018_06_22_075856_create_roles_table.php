25d4b1284b0e4d2cd496f0c15cddcc14b5425b15
saoriii <38948379+saoriii@users.noreply.github.com>
2018-06-22 10:19:06 +0200

    Update 2018_06_22_075856_create_roles_table.php

--- database/migrations/2018_06_22_075856_create_roles_table.php
+++ database/migrations/2018_06_22_075856_create_roles_table.php
@@ -15,8 +15,8 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->timestamps();
             $table->string('name');
+            $table->timestamps();
         });
     }
 
