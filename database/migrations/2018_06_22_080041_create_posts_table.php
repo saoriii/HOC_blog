6ae398d10f89ee576f890f4970805209b2d831a6
Faresse Tahlaiti <faresse.tahlaiti@aol.com>
2018-06-22 11:22:18 +0200

    Modifie table posts

--- database/migrations/2018_06_22_080041_create_posts_table.php
+++ database/migrations/2018_06_22_080041_create_posts_table.php
@@ -17,9 +17,9 @@ class CreatePostsTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->text('content');
-            $table->integer('user_id');
-            $table->integer('photo_id');
-            $table->integer('category_id');
+            $table->integer('user_id')->index()->nullable()->unsigned();
+            $table->integer('photo_id')->index()->nullable()->unsigned();
+            $table->integer('category_id')->index()->nullable()->unsigned();
 
 
 
--- database/migrations/2018_06_22_080358_create_comments_table.php
+++ database/migrations/2018_06_22_080358_create_comments_table.php
@@ -1,4 +1,4 @@
-<?php
+f<?php
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
