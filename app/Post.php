245fc345571e7aea1d909761fbc71f414e07b386
saoriii <saori.kido07@gmail.com>
2018-06-28 16:11:35 +0200

    Models User, Role, Post

---
+++ app/Post.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    protected $fillable = [
+        'title',
+        'content',
+        'user_id',
+        'photo_id',
+        'category_id'
+    ];
+
+    public function user() {
+        return $this->belongsTo('App\User');
+    }
+
+    public function comments() {
+        return $this->hasMany('App\Comment');
+    }
+
+    public function category() {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function photos() {
+        return $this->morphMany('App\Photo');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -26,4 +26,14 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function roles() {
+        return $this->belongsToMany('App\Role');
+
+    }
+
+    public function photos() {
+        return $this->morphMany('App\Photo');
+    }
+
 }
