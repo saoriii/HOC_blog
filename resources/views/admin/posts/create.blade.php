113f3f4389c79d7e4640105de05ac04f2385dced
saoriii <saori.kido07@gmail.com>
2018-06-29 09:56:46 +0200

    Views create et edit

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,13 +1,20 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create</title>
-</head>
-<body>
+@extends('layouts.app')
 
-</body>
-</html>
+@section('content')
+
+    {!! Form::open(['method' => 'POST', 'action' => 'AdminPostsController@store']) !!}
+
+    {!! Form::label("title", "Titre") !!}
+    {!! Form::text("title", null) !!}<br />
+
+    {!! Form::label("content", "Contenu") !!}
+    {!! Form::textarea("content", null) !!}<br />
+
+    {!! Form::label("is_active", "Affichage") !!}
+    {!! Form::select("is_active", ["0" => "Inactif", "1" => "Actif"]) !!}<br />
+
+    {!! Form::submit("Envoyer") !!}
+
+    {!! Form::close() !!}
+
+@stop
