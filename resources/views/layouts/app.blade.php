ac1f41abf821e608ee71ac124ef8e2f6d4a4437e
saoriii <saori.kido07@gmail.com>
2018-07-19 09:35:23 +0200

    tacoca vers admin

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="{{ url('/admin') }}">
                         TaCoCaFa
                     </a>
                 </div>
