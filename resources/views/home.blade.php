377c414360abb6fbca8ec992991a2e42336a34f8
Faresse TAHLAITI <faresse.tahlaiti@aol.com>
2018-07-20 13:59:17 +0200

    comit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,4 +20,36 @@
     </div>
   </div>
 </div>
+
+<ul class="collapsible popout">
+    <li>
+      <div class="collapsible-header"><i class="material-icons">filter_drama</i>Article récent</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">place</i>Lieux</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+    <li>
+      <div class="collapsible-header"><i class="material-icons">whatshot</i>Hot</div>
+      <div class="collapsible-body"><span>Lorem ipsum dolor sit amet.</span></div>
+    </li>
+  </ul>
 @endsection
+
+
+@section("scripts")
+<script>
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.collapsible ').collapsible();
+  });
+</script> 
+
+@stop
