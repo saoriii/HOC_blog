0146d23e22c8911fe3dfd7d12d20f1a7e119bfff
Faresse TAHLAITI <faresse.tahlaiti@aol.com>
2018-07-20 09:52:29 +0200

    seconde commit

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-    <p><a class="waves-effect waves-light btn cardiff" href="{{route('posts.index')}}">Gérer les posts</a></p>
+    <p><a class="waves-effect waves-light btn greyss" href="{{route('posts.index')}}">Gérer les posts</a></p>
     <p><a class="waves-effect waves-light btn greyss" href="{{route('categories.index')}}">Gérer les catégories</a></p>
     <p><a class="waves-effect waves-light btn greyss" href="{{route('users.index')}}">Gérer les utilisateurs</a></p>
-    <p><a class="waves-effect waves-light btn coucou" href="{{route('comments.index')}}">Gérer les commentaires</a></p>
-    <p><a class="waves-effect waves-light btn coucou" href="{{route('medias.index')}}">Gérer les photos</a></p>
+    <p><a class="waves-effect waves-light btn greyss" href="{{route('comments.index')}}">Gérer les commentaires</a></p>
+    <p><a class="waves-effect waves-light btn greyss" href="{{route('medias.index')}}">Gérer les photos</a></p>
 
 @stop
